9bfd6d217f84c13dd7c12fc594c889e30086ee52
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-02 09:13:53 +0300

    Working with numbers

---
+++ JavaScript/Numbers/numbers.js
@@ -0,0 +1,12 @@
+const ironManStars = 4
+const thorStars = 5
+const captainAmericaStars = 3
+const avengerStars = 1
+const hulkStars = 2
+
+const mcuTotalStars = ironManStars + thorStars + captainAmericaStars + avengerStars + hulkStars
+
+const mcuAverageStars = mcuTotalStars / 5
+
+console.log(mcuTotalStars)
+console.log(mcuAverageStars)
