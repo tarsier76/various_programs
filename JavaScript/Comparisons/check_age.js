d4d5ffb5625721da49e2f4e33318d074e2fd5cc9
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-03 00:03:02 +0300

    Check number values using if statements

---
+++ JavaScript/Comparisons/check_age.js
@@ -0,0 +1,14 @@
+const bobbyAge = 17
+const jillAge = 19
+
+if (bobbyAge >= 18) {
+    console.log('Bobby is old enough')
+} else {
+    console.log('Bobby is not old enough')
+}
+
+if (jillAge >= 18) {
+    console.log('Jill is old enough')
+} else {
+    console.log('Jill is not old enough')
+}
