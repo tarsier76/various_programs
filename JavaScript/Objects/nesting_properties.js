673692ae3dfa77ab2668ea71fda3d89dcd04f8db
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-05 14:43:04 +0300

    Nesting object properties

---
+++ JavaScript/Objects/nesting_properties.js
@@ -0,0 +1,11 @@
+const review = {
+    text: 'This movie was awful',
+    stars: 2,
+    author: {
+      firstName: 'Johnny',
+      lastName: 'Comelately',
+      createdAt: '2022-08-17T15:41:25+00:00'
+    }
+  }
+  
+console.log(review.author.firstName)
