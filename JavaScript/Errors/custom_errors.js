bb8d8b4c22b77c194ad00fc5bdf769bf4bd63121
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-06 20:07:33 +0300

    Creating and throwing custom errors

---
+++ JavaScript/Errors/custom_errors.js
@@ -0,0 +1,33 @@
+function getMovieRecord(movieId) {
+    if (movieId === 1) {
+      return { name: 'Apollo 13', duration: 128 }
+    }
+    if (movieId === 2) {
+      return { name: '2001: A Space Odyssey', duration: 300 }
+    }
+    if (movieId === 3) {
+      return { name: 'Interstellar', duration: 4000 }
+    }
+    throw new Error('Movie id not found.')
+  }
+  
+  function main() {
+    try {
+      logObject(getMovieRecord(1))
+      logObject(getMovieRecord(2))
+      logObject(getMovieRecord(3))
+      logObject(getMovieRecord(4))
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+  
+  function logObject(obj) {
+    for (const key in obj) {
+      console.log(` - ${key}: ${obj[key]}`)
+    }
+    console.log('---')
+  }
+  
+  main()
+  
