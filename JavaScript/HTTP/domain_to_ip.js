85f5ba246fee7838247fac7b6e444ae578448fa7
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-07 15:11:24 +0300

    Return IP address from json response

---
+++ JavaScript/HTTP/domain_to_ip.js
@@ -0,0 +1,24 @@
+async function fetchIPAddress(domain) {
+    const resp = await fetch(`https://cloudflare-dns.com/dns-query?name=${domain}&type=A`, {
+      headers: {
+        'accept': 'application/dns-json'
+      }
+    });
+    const respObject = await resp.json();
+  
+    return respObject.Answer[0].data
+  }
+  
+  // don't touch below this line
+  async function main() {
+    const domain = 'example.com';
+    const ipAddress = await fetchIPAddress(domain);
+    if (!ipAddress) {
+      console.log('something went wrong in fetchIPAddress');
+    } else {
+      console.log(`found IP address for domain ${domain}: ${ipAddress}`);
+    }
+  }
+  
+  main();
+  
