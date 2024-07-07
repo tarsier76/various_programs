c6ffebc1cc55292decdb55bfca1ee5307e30f392
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-07 15:35:23 +0300

    Get domain name from URL

---
+++ JavaScript/HTTP/get_domain_name.js
@@ -0,0 +1,8 @@
+function getDomainNameFromURL(url) {
+    return new URL(url).hostname
+}
+  
+  const bootdevURL = 'https://boot.dev/learn/learn-python'
+  const domainName = getDomainNameFromURL(bootdevURL)
+  console.log(`The domain name for ${bootdevURL} is ${domainName}`)
+  
