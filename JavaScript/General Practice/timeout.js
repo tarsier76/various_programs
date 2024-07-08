d05e3fdfad1ed5c7b5bbb684e90d4d693dd7db66
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-08 12:30:16 +0300

    Set timeout between print statements

---
+++ JavaScript/General Practice/timeout.js
@@ -0,0 +1,11 @@
+const craftingCompleteWait = 8000
+const combiningMaterialsWait = 6000
+const smeltingIronBarsWait = 4000
+const shapingIronWait = 2000
+
+setTimeout(() => console.log('Iron Longsword Complete!'), craftingCompleteWait)
+setTimeout(() => console.log('Combining Materials...'), combiningMaterialsWait)
+setTimeout(() => console.log('Smelting Iron Bars...'), smeltingIronBarsWait)
+setTimeout(() => console.log('Shaping Iron...'), shapingIronWait)
+
+console.log('Firing up the forge...')
