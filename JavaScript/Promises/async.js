c02db67d660b051e445fdb0795a81fb73a0477ba
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-08 16:11:24 +0300

    Using async on a function

---
+++ JavaScript/Promises/async.js
@@ -0,0 +1,30 @@
+async function getItemData() {
+    const response = await fetch('https://api.boot.dev/v1/courses_rest_api/learn-http/items', getSettings())
+    return response.json()
+  }
+  
+async function main() {
+  const items = await getItemData()
+
+  logItems(items)
+}
+
+function getSettings() {
+  return {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'X-API-Key': 'Testing',
+      'Content-Type': 'application/json'
+    }
+  }
+}
+
+function logItems(items) {
+  for (const item of items) {
+    console.log(item.name)
+  } 
+}
+
+main()
+  
