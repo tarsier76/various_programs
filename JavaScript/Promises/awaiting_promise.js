79098aacac56bc53f3b634bdce7018e8b4ffc705
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-08 15:45:46 +0300

    Using await

---
+++ JavaScript/Promises/awaiting_promise.js
@@ -0,0 +1,16 @@
+async function main() {
+message = await applyDamage(25, 50)
+
+console.log(message)
+}
+
+function applyDamage(damage, currentHP) {
+  return new Promise((resolve) => {
+    const newHP = currentHP - damage
+    setTimeout(() => {
+      resolve(`The player with ${currentHP} hit points suffers ${damage} points of damage and has ${newHP} hit points remaining.`)
+    }, 1000)
+  })
+}
+
+main()
