dbd889438eb90a4a968613d05986204a896e054f
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-08 18:40:35 +0300

    Printing error message without extra formatting

---
+++ JavaScript/Errors/handling_error.js
@@ -0,0 +1,14 @@
+try {
+printCharacterStats(4)
+printCharacterStats('ten')
+printCharacterStats(10)
+} catch (err) {
+    console.log(`${err.message}`)
+}
+
+function printCharacterStats(level) {
+  if (isNaN(level)) {
+    throw new Error('Parameter is not a number!')
+  }
+  console.log(`Your character is level ${level}!`)
+}
