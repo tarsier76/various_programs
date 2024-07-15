b0b855c27fc0ca72c4d69960fbbf77acde335ffa
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-15 16:33:23 +0300

    Class methods

--- JavaScript/OOP/classes.js
+++ JavaScript/OOP/classes.js
@@ -3,7 +3,16 @@ class User {
         this.email = email 
         this.name = name 
     }
+    login() {
+        console.log(this.email, 'just logged in ')
+    }
+    logout() {
+        console.log(this.email, 'just logged out')
+    }
 }
 
 let userOne = new User("Ryu@ninjas.com", "Ryu")
 let userTwo = new User("Yo@gmail.com", "Bob")
+
+userOne.login()
+userTwo.logout()
