7ac74345f070d40b7e35eb95f4dfc0c069b29511
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-15 16:28:41 +0300

    Using classes

---
+++ JavaScript/OOP/classes.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(email, name) {
+        this.email = email 
+        this.name = name 
+    }
+}
+
+let userOne = new User("Ryu@ninjas.com", "Ryu")
+let userTwo = new User("Yo@gmail.com", "Bob")
