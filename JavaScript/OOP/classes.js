e05cd879ca7a57611716aded257797413571839a
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-15 16:39:02 +0300

    Method chaining

--- JavaScript/OOP/classes.js
+++ JavaScript/OOP/classes.js
@@ -2,17 +2,24 @@ class User {
     constructor(email, name) {
         this.email = email 
         this.name = name 
+        this.score = 0
     }
     login() {
         console.log(this.email, 'just logged in ')
+        return this 
     }
     logout() {
         console.log(this.email, 'just logged out')
+        return this
+    }
+    updateScore() {
+        this.score++
+        console.log(this.email, 'score is now', this.score)
+        return this 
     }
 }
 
 let userOne = new User("Ryu@ninjas.com", "Ryu")
 let userTwo = new User("Yo@gmail.com", "Bob")
 
-userOne.login()
-userTwo.logout()
+userOne.login().updateScore().updateScore().logout()
